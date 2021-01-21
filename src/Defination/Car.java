ccd53efd33bd3770d18df8e85c10214952992b7f
Harshil Gupta <harshilgupta0099@gmail.com>
2021-01-21 14:35:02 +0530

    add checkSpeed Method in Car class

--- src/Defination/Car.java
+++ src/Defination/Car.java
@@ -7,6 +7,16 @@ public class Car {
         this.speed= speed;
     }
     private final double  MAXIMUM_SPEED = 200;
+    public void checkSpeed(){
+        if(this.speed>=MAXIMUM_SPEED)
+        {
+            throw new MaximumSpeedException("!!!!!Over Speed Warning !!!!! Please Slow down your Car");
+        }
+        else {
+            System.out.println("Drive Safe :)");
+
+        }
+    }
 
 
 
