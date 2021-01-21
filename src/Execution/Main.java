bf98218ba14231c1224dd9fb4ca4595069b03918
Harshil Gupta <harshilgupta0099@gmail.com>
2021-01-21 14:39:08 +0530

    add car object in main class and add checkSpeed method

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -1,7 +1,11 @@
 package Execution;
 
+import Defination.Car;
+
 public class Main {
     public static void main(String[] args) {
+        Car carObject= new Car(201);
+        carObject.checkSpeed();
 
     }
 
